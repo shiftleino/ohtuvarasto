46490feeaa34504f32601d9452473cde8678ded5
shiftleino <shiftleino@gmail.com>
2022-10-29 11:09:20 +0300

    Remove syntax error, add new error

--- src/varasto.py
+++ src/varasto.py
@@ -4,7 +4,7 @@ class Varasto:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
-            self.tilavuus = 0.0
+            self.tilavuus = 100
         
         if alku_saldo < 0.0:
             # virheellinen, nollataan
