63962b038177fb16cfdbd7b72c2ee2ab896f8e89
shiftleino <shiftleino@gmail.com>
2022-10-29 11:11:09 +0300

    Fix new error

--- src/varasto.py
+++ src/varasto.py
@@ -4,7 +4,7 @@ class Varasto:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
-            self.tilavuus = 100
+            self.tilavuus = 0.0
         
         if alku_saldo < 0.0:
             # virheellinen, nollataan
